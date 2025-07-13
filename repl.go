2a4dc76c14c6387a6d74d5961376c4fd30d5a72b
lymvs <lysandros.mavropoulos@gmail.com>
2025-07-13 19:37:44 +0000

    Add start REPL func

--- repl.go
+++ repl.go
@@ -1,6 +1,11 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	var words []string
@@ -8,4 +13,21 @@ func cleanInput(text string) []string {
 	words = strings.Fields(strings.ToLower(text))
 
 	return words
+}
+
+func startRepl() {
+	prompt := "Pokedex > "
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(prompt)
+		scanner.Scan()
+
+		input := cleanInput(scanner.Text())
+		fmt.Printf("Your command was: %s\n", input[0])
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+	}
 }
